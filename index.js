145548fc7900e85c887b5bc9ee9c095c2c882fd7
JakeRArent <arentjake92@gmail.com>
2024-04-26 20:54:28 -0500

    Class Examples

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+import _ from 'lodash';
 import { color, log, red, green } from 'console-log-colors';
+ 
+const myarray = [1, 2, 3, 4, 5, 8, 4, 2, 9 ];
 
-console.log (color.red("This is NOT a boring message."));
-console.log (color.green("This is REALLY NOT a boring message"));
+console.log(color.red(myarray));
+ 
+const output = _.uniq(myarray);
+
+console.log(output);
+
