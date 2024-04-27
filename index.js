125ebdb376ee2ff5a91198e70c52ad768565408b
JakeRArent <arentjake92@gmail.com>
2024-04-26 21:53:53 -0500

    Show file system stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import fs from 'fs';
 import _ from 'lodash';
 import { color, log, red, green } from 'console-log-colors';
  
 const myarray = [1, 2, 3, 4, 5, 8, 4, 2, 9 ];
+
 
 console.log(color.red(myarray));
  
@@ -9,3 +11,15 @@ const output = _.uniq(myarray);
 
 console.log(output);
 
+// echo 'node_modules/*' > .gitignore
+// git add -A
+// git commit -m "Some message"
+
+// Sync File Access
+const file = fs.readFileSync('./data.json', {encoding: 'utf8'});
+console.log(color.red(file));
+
+
+
+// An ASync File Access method
+fs.readFile('./data.json', {encoding: 'utf-8'}, (err,f) => console.log(color.green(f)));
